a0724650f29de2d22f0d5bd3e6b2eb4f9956b881
DdogRing <1937083398@qq.com>
2021-02-23 13:46:01 +0800

    2021年2月23日13:45:37

--- src/main/java/com/ddogring/homepage/HomepageApplication.java
+++ src/main/java/com/ddogring/homepage/HomepageApplication.java
@@ -6,7 +6,6 @@ import org.apache.tomcat.util.descriptor.web.SecurityCollection;
 import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
